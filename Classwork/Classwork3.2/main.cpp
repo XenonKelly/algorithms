a5dde1b6221adff5638047cc7a3dbe570f155475
Ksenia Likhoshertova <lihocherdtova@gmail.com>
2023-09-30 13:35:08 +0300

    Update main.cpp

--- Classwork/Classwork3.2/main.cpp
+++ Classwork/Classwork3.2/main.cpp
@@ -1,7 +1,5 @@
 ﻿#include <iostream>
 #include<vector>
-#include<algorithm>
-#include<iterator>
 #include <random>
 
 using namespace std;
@@ -23,35 +21,47 @@ int main()
         v.push_back(input);
     }
  
-    vector<int>::iterator ip; //для поиска дубликатов чисел в массиве
+    int k = *std::max_element(v.begin(), v.end()) + 1;
+    
+    vector<int> count(k, 0);
+    for (auto x : v) {
+        count[x] += 1;
+    }
+
+    for (int i = 1; i < k; i++) {
+        count[i] += count[i - 1];
+    }
+
+  
     /*vector<int> v(100); //изначальный массив
     mt19937 mt(time(nullptr));
     for (int i = 0; i < v.size(); i++)
         v[i] = (mt() % 100) + 100;*/
     
-    sort(v.begin(), v.end());
-
-    ip = unique(v.begin(), v.begin() + v.size());
-    v.resize(distance(v.begin(), ip));
 
     cout << "Input q:\n";
     int q;
     cin >> q;
 
     cout << "Input l and r:\n";
-    int l, r;
+    int l, r, res;
 
     for (int i = 0; i < q; i++)
     {
         cin >> l >> r;
 
-        std::vector<int>::iterator a_it; //начало отрезка
-        std::vector<int>::iterator b_it; //конец отрезка
-        a_it = lower_bound(v.begin(), v.end(), l);
-        b_it = upper_bound(v.begin(), v.end(), r);
-        cout << b_it - a_it;
+        if ((l == 0) && (r >= k))
+            res = count.back();
+        else if ((l > 0) && (r >= k))
+            res = count.back() - count[l - 1];
+        else if (l == 0 and r < k)
+            res = count[r];
+        else
+            res = count[r] - count[l - 1];
+
+        cout << res;
         cout << endl;
-    } 
+    }
 }
 
 
